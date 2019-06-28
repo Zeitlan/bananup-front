56fbedeb9f0982c0733e8593c4e4079ad61c4575
Antoine Léauté <leaute_a@epita.fr>
2019-06-28 19:03:51 +0200

    fix: remove extra / in app-config

--- src/app-config.js
+++ src/app-config.js
@@ -5,6 +5,6 @@ const appConfig = {
     apiUrl: process.env.REACT_APP_API_URL || 'http://127.0.0.1:8000',
 }
 
-appConfig.authUrl = `${appConfig.apiUrl}/auth/`
+appConfig.authUrl = `${appConfig.apiUrl}/auth`
 
 export default appConfig
