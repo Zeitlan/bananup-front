62990d32378648bd4ec3bae0c17a9c2577d4e2fc
Valentin Ouvrard <valentin.ouvrard@yahoo.fr>
2019-09-24 15:58:57 +0200

    feat(categories): add globale categories

--- src/components/categories/category-list.js
+++ src/components/categories/category-list.js
@@ -2,35 +2,27 @@ import React from 'react'
 import styled from 'styled-components'
 import Category from './category'
 import { Grid } from '@material-ui/core'
-import PropTypes from 'prop-types'
+import { useQuery } from '@apollo/react-hooks';
+import { PREVIEW_CATEGORIES } from './category-queries.js'
 
 
-class CategoryListPreview extends React.Component {
-    
-  componentDidMount() {
-      const { actions: { getCategories }} = this.props
-      getCategories()
-  }
-    
-  render() {
-    const { state: {categories}} = this.props
-    return (
-        <Container>
-            <Grid container spacing={3}>
-              {categories && categories.map((category) => 
+function CategoryListPreview() {
+  const { loading, error, data } = useQuery(PREVIEW_CATEGORIES);
+
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return (
+      <Container>
+          <Grid container spacing={3}>
+            {data.allCategories.map((category) => 
               <Grid key={category.id} item xs={6} sm={4} md={3} lg={2}>
                   <Category category={category}/>
               </Grid>)}
-            </Grid>
-        </Container>
-    )
-  }
-}
-
-// PropTypes
-CategoryListPreview.propTypes = {
-    actions: PropTypes.object.isRequired,
-    state: PropTypes.object.isRequired
+          </Grid>
+      </Container>
+  )
 }
 
 // Styles
--- src/components/categories/category-queries.js
+++ src/components/categories/category-queries.js
@@ -11,4 +11,3 @@ export const PREVIEW_CATEGORIES = gql`
     }
   }
 `
-
